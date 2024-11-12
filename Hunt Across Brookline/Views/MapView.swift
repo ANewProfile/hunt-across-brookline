b9febe586fbe8cbe85570eb4f317f7dbabbefde6
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-11-12 15:20:56 -0500

    wooooooo first coding session 🚀

---
+++ Hunt Across Brookline/Views/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  Hunt Across Brookline
+//
+//  Created by Theo Chen on 11/12/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+
+    var body: some View {
+        Map(position: .constant(.region(region)))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.0025, longitudeDelta: 0.0025)
+        )
+    }
+}
+
+#Preview {
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+}
